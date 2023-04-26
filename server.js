6ca290f367a22edfd67b0dc2481cf286c94661fa
ericpkatz <ericpkatz@gmail.com>
2023-04-26 12:25:57 -0400

    added 2 routes

--- server.js
+++ server.js
@@ -3,18 +3,72 @@ const app = express();
 const pg = require('pg');
 const client = new pg.Client('postgres://localhost/acme_clothing_db');
 
-app.get('/', async(req, res, next)=> {
+
+app.get('/', (req, res)=> res.redirect('/products'));
+
+app.get('/products', async(req, res, next)=> {
   try {
     const SQL = `
       SELECT *
-      FROM products;
+      FROM products
+      ORDER BY name
     `;
     const response = await client.query(SQL); 
     const products = response.rows;
-    res.send(products);
+    res.send(`
+      <html>
+        <head>
+          <title>Acme Clothing</title>
+        </head>
+        <body>
+          <h1>The Acme Clothing Company</h1>
+          <ul>
+            ${
+              products.map( product => {
+                return `
+                  <li>
+                    <a href='/products/${product.id}'>
+                      ${ product.name }
+                    </a>
+                  </li>`;
+              }).join('')
+            }
+          </ul>
+        </body>
+      </html>
+    `);
 
   }
   catch(ex){
+    console.log(ex);
+    next(ex);
+  }
+});
+
+app.get('/products/:id', async(req, res, next)=> {
+  try {
+    const SQL = `
+      SELECT *
+      FROM products
+      WHERE id = $1
+    `;
+    const response = await client.query(SQL, [ req.params.id ]); 
+    const product = response.rows[0];
+    res.send(`
+      <html>
+        <head>
+          <title>Acme Clothing</title>
+        </head>
+        <body>
+          <h1>The Acme Clothing Company</h1>
+          <h2><a href='/products'>${ product.name }</a></h2>
+        </body>
+      </html>
+    `);
+
+  }
+  catch(ex){
+    console.log(ex);
     next(ex);
   }
 });
